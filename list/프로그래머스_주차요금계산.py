2927b98362e3cf178d4c3000f32ae9ae28167157
strongcookdas <goodjav141592@gmail.com>
2023-07-24 17:36:49 +0900

    feat : 프로그래머스_주차요금계산

--- list/프로그래머스_주차요금계산.py
+++ list/프로그래머스_주차요금계산.py
@@ -12,6 +12,7 @@ OUT일 경우
 계산된 시간 딕셔너리를 가지고 요금 계산후 반환
 
 '''
+'''
 import math
 
 
@@ -66,3 +67,38 @@ print(solution([180, 5000, 10, 600], ["05:34 5961 IN", "06:00 0000 IN", "06:34 0
                                       "07:59 0148 IN", "18:59 0000 IN", "19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT"]))
 
 # [14600, 34400, 5000]
+'''
+
+# 강의대로 따라해봤지만 통과가 안됨
+'''
+import math
+
+def solution(fees, records):
+    answer = []
+    inTime = [0]*10000
+    isIn = [0]*10000
+    sumT = [0]*10000
+
+    for record in records:
+        a, b, c = record.split()
+        h, m = a.split(':')
+        if c == 'IN':
+            inTime[int(b)] = int(h)*60+int(m)
+            isIn[int(b)] = 1
+        else:
+            sumT[int(b)] += (int(h)*60*int(m)) - inTime[int(b)]
+            isIn[int(b)] = 0
+
+    for i in range(10000):
+        if isIn[i] == 1:
+            sumT[i] += (23*60+59) - inTime[i]
+
+    # 요금 계산
+    for i in range(10000):
+        if sumT[i] > 0:
+            answer.append(
+                fees[1]+max(math.ceil((sumT[i]-fees[0])/fees[2]), 0)*fees[3])
+
+    return answer
+
+    '''
